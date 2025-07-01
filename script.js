36612768aa920cb3efc622d0f352ec1f253b79a6
Aaron Marty <aaronmarty98@gmail.com>
2025-07-01 10:07:57 -0400

    Update script.js
    Testimonila Js

--- script.js
+++ script.js
@@ -42,3 +42,30 @@ $('.main_slider').slick({
     }
   ]
 });
+
+
+
+$(document).ready(function(){
+    $('.testimonial-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 4000,
+      dots: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
